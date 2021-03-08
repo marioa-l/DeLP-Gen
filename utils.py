46ad01dd541429323e044d7194a4e248b21af0fc
Mario Alejandro Leiva <mario.leiva.al@gmail.com>
2021-03-08 15:51:39 -0300

    Utils reforms

--- utils.py
+++ utils.py
@@ -19,6 +19,15 @@ class Utils:
         
     
     def print_msj(self, msg_type: str, text: str) -> None:
+        """
+        To print messages with differents colors
+        Args:
+            -msg_type: 
+                'Error': To show an error
+                'Info': To show information
+                'OK': To show correct outputs
+            -text: The text to be printed
+        """
         if msg_type == 'ERROR':
             print(bcolors.FAIL + text + bcolors.ENDC)
         elif msg_type == 'INFO':
@@ -26,13 +35,33 @@ class Utils:
         elif msg_type == 'OK':
             print(bcolors.OKGREEN + text + bcolors.ENDC)
 
-    
+   
+    def to_string_decimal_format(self, number: float) -> str:
+        """
+        To convert a float number into string (changing the '.' with ',')
+        Args:
+            -number: The float number to be converted
+        """
+       return str(float('{0:.2f}'.format(number))).replace('.',',')
+
+
     def write_result(self, path_file: str, result: json) -> None:
+        """
+        To write the results in a json file
+        Args:
+            -path_file: The full path of the file to be created
+            -result: The json data to be saved
+        """
         with open(path_file, 'w') as output:
             json.dump(result, output, indent=4)
 
     
-    def get_data_from_file(self, path_file):
+    def get_data_from_file(self, path_file: str) -> json:
+        """
+        To read a json file
+        Args:
+            -path_file: The path of the file
+        """
         try:
             file = open(path_file, "r")
             toDict = json.load(file)
@@ -48,3 +77,26 @@ class Utils:
             self.print_msj("ERROR", "JSON incorrect format: %s" 
                             % (path_file))
             exit()
+
+
+    def write_metrics(self, result_path_file: str):
+        """
+        To comput the average of all metrics and save in a
+        simple txt file (to use in excel)
+        Args:
+            result_path_files: The path of the file with all results
+        """
+        data = self.get_data_from_file(result_path_file)
+        arguments = sum(data['arguments']) / len(data['arguments'])
+        def_rules = sum(data['def_rules']) / len(data['def_rules'])
+        arg_lines = sum(data['arg_lines']) / len(data['arg_lines']) 
+        height_lines = sum(data['height_lines']) / len(data['height_lines'])
+
+        to_write = ('Arguments MDDL H T\n'
+                    + self.to_string_decimal_format(arguments)
+                    + ' ' + self.to_string_decimal_format(def_rules)
+                    + ' ' + self.to_string_decimal_format(height_lines)
+                    + ' ' + self.to_string_decimal_format(arg_lines))
+
+        with open('./metrics.txt', 'w') as fileOutput:
+            fileOutput.write(to_write)
